7f814fbfc94a4bc744b250bf06972ec9a221e74c
Łukasz Kita <lukasz.kita0@gmail.com>
2024-03-15 13:46:56 +0100

    Fix an artefact occuring if the position of an overlay is not :center or (0, 0). Bump to v0.1.1 (#2)

--- test/membrane_overlay_filter_test.exs
+++ test/membrane_overlay_filter_test.exs
@@ -4,7 +4,7 @@ defmodule Membrane.OverlayFilterTest do
   import Membrane.Testing.Assertions
   alias Membrane.Testing
 
-  test "applies overlay on top of a YUV frame" do
+  test "applies overlay on a given position of a YUV frame" do
     pipeline =
       Testing.Pipeline.start_link_supervised!(
         spec:
@@ -12,12 +12,34 @@ defmodule Membrane.OverlayFilterTest do
             output: [File.read!("test/fixtures/frame.yuv")]
           })
           |> child(%Membrane.RawVideo.Parser{width: 1920, height: 1080, pixel_format: :I420})
-          |> child(%Membrane.OverlayFilter{overlay: "test/fixtures/overlay.png"})
+          |> child(%Membrane.OverlayFilter{overlay: "test/fixtures/overlay.png", x: 200, y: 100})
           |> child(:sink, Membrane.Testing.Sink)
       )
 
     assert_sink_buffer(pipeline, :sink, buffer)
     assert buffer.payload == File.read!("test/fixtures/output.yuv")
+
+    Testing.Pipeline.terminate(pipeline)
+  end
+
+  test "applies overlay on a center of a YUV frame" do
+    pipeline =
+      Testing.Pipeline.start_link_supervised!(
+        spec:
+          child(%Testing.Source{
+            output: [File.read!("test/fixtures/frame.yuv")]
+          })
+          |> child(%Membrane.RawVideo.Parser{width: 1920, height: 1080, pixel_format: :I420})
+          |> child(%Membrane.OverlayFilter{
+            overlay: "test/fixtures/overlay.png",
+            x: :middle,
+            y: :center
+          })
+          |> child(:sink, Membrane.Testing.Sink)
+      )
+
+    assert_sink_buffer(pipeline, :sink, buffer)
+    assert buffer.payload == File.read!("test/fixtures/output_center.yuv")
 
     Testing.Pipeline.terminate(pipeline)
   end
